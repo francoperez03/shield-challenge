0b59d58c3e8ce9affb14d876a42d8b64ff69893a
francoperez03 <franco.perez03@gmail.com>
2024-11-14 08:27:28 -0300

    first commit

---
+++ ormconfig.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: any = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'wallet_db',
+  synchronize: false,
+  logging: false,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export = config;
